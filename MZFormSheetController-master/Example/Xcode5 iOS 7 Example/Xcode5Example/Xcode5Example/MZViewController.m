dad2fbecd0d40e0d9b9710060affe50b18b32324
Eichto <MAEDi@Marks-Mac-mini.local>
2013-10-15 13:27:31 +0800

    Added a Sliding Function in IOS Development.

---
+++ MZFormSheetController-master/Example/Xcode5 iOS 7 Example/Xcode5Example/Xcode5Example/MZViewController.m
@@ -0,0 +1,44 @@
+//
+//  MZViewController.m
+//  Xcode5Example
+//
+//  Created by Michał Zaborowski on 13.08.2013.
+//  Copyright (c) 2013 Michał Zaborowski. All rights reserved.
+//
+
+#import "MZViewController.h"
+#import "MZFormSheetController.h"
+
+@interface MZViewController ()
+
+@end
+
+@implementation MZViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    
+    [[MZFormSheetBackgroundWindow appearance] setBackgroundBlurEffect:YES];
+    [[MZFormSheetBackgroundWindow appearance] setBlurRadius:5.0];
+    [[MZFormSheetBackgroundWindow appearance] setBackgroundColor:[UIColor clearColor]];
+}
+
+- (IBAction)showFormSheet:(UIButton *)sender
+{
+    UIViewController *vc = [self.storyboard instantiateViewControllerWithIdentifier:@"modal"];
+    
+    MZFormSheetController *formSheet = [[MZFormSheetController alloc] initWithViewController:vc];
+
+    formSheet.transitionStyle = MZFormSheetTransitionStyleSlideFromTop;
+    formSheet.shadowRadius = 2.0;
+    formSheet.shadowOpacity = 0.3;
+    formSheet.shouldDismissOnBackgroundViewTap = YES;
+    formSheet.shouldCenterVerticallyWhenKeyboardAppears = YES;
+    
+    [formSheet presentAnimated:YES completionHandler:^(UIViewController *presentedFSViewController) {
+        
+    }];
+}
+
+@end
